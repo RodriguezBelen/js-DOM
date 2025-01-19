b3ad55493c6aae6d5e4de3480307e67c3fb98620
Rodriguezbelen <beelu2.1997@gmail.com>
2025-01-19 14:10:06 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// const links = document.querySelector('#links');
+// console.log(links);
+
+const image = document.querySelector('nav>img');
+// console.log(image.alt);
+image.alt = 'Icono'
+
+const link1 = document.querySelector('#link1');
+console.log(link1)
+link1.innerText = 'Home'
