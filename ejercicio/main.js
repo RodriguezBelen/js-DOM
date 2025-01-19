c4d22f2fbf6c0e58cfa2040ebeca7e5a885a18e1
Rodriguezbelen <beelu2.1997@gmail.com>
2025-01-19 19:57:53 -0300

    nuevas lineas

--- ejercicio/main.js
+++ ejercicio/main.js
@@ -1,6 +1,17 @@
-const boton = document.querySelector('#miBoton');
-const parrafo = document.querySelector('p')
+// const boton = document.querySelector('#miBoton');
+// const parrafo = document.querySelector('p')
+    
+// boton.addEventListener('click', function() {
+// parrafo.classList.toggle('fondo-verde')
+// })
 
-boton.addEventListener('click', function() {
-    parrafo.classList.toggle('fondo-verde')
+const formulario = document.querySelector('form')
+
+formulario.addEventListener('submit', function(event){
+    event.preventDefault()
+    // console.log('El formulario fue enviado')
+    const datos = Object.fromEntries( new FormData(event.target))
+    console.log(datos)
+
 })
+
